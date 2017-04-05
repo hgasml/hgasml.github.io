ebea3f8b5db025aefc273df2824c6aee5afdd659
hgasml <hga_sml@mail.ru>
2017-04-05 21:49:09 +0200

    Main Site

---
+++ atcs/zoom_pageinfo.js
@@ -0,0 +1,16 @@
+pageinfo = [[0,0,0,0,null],
+	[0,0,0,0,null],
+	[0,0,0,0,null],
+	[0,0,0,0,null],
+	[0,0,0,0,null],
+	[0,0,0,0,null],
+	[0,0,0,0,null],
+	[0,0,0,0,null]];
+pagedata = [ ["atcs_ftsearch.htm","Georg Smail Present","Enter one or more keywords to search (&#39;*&#39; and &#39;?&#39; wildcards are supported): You must have JavaScript enabled to use this version of the search eng...",""],
+["introduction.htm","1: Introduction","1: Introduction Navigation: No topics above this level 1: Introduction Tutorial level: Advanced Requirements: You should be familiar with using th...",""],
+["twoversions.htm","Tricks for Experts &#62; Two Versions of the Same Topic","Tricks for Experts Two Versions of the Same Topic Navigation: Tricks for Experts Two Versions of the Same Topic Tutorial level: Advanced Requireme...",""],
+["twoversions1.htm","Tricks for Experts &#62; Two Versions of the Same Topic &#62; 1: Define your Build Options","Tricks for Experts Two Versions of the Same Topic 1: Define your Build Options Navigation: Tricks for Experts Two Versions of the Same Topic 1: De...",""],
+["twoversions2.htm","Tricks for Experts &#62; Two Versions of the Same Topic &#62; 2: Create the Topic Projects","Tricks for Experts Two Versions of the Same Topic 2: Create the Topic Projects Navigation: Tricks for Experts Two Versions of the Same Topic 2: Cr...",""],
+["twoversions3.htm","Tricks for Experts &#62; Two Versions of the Same Topic &#62; 3: Link to the Topic","Tricks for Experts Two Versions of the Same Topic 3: Link to the Topic Navigation: Tricks for Experts Two Versions of the Same Topic 3: Link to th...",""],
+["twoversions4.htm","Tricks for Experts &#62; Two Versions of the Same Topic &#62; 4: Publish Conditionally","Tricks for Experts Two Versions of the Same Topic 4: Publish Conditionally Navigation: Tricks for Experts Two Versions of the Same Topic 4: Publis...",""],
+["vvedenie.htm","Введение","Краткое описание Navigation: No topics above this level Краткое описание ЦЕНТРАЛЬНЫЙ УПРАВЛЕНЧЕСКИЙ ПУНКТ АСУД (ЦУП АСУД) Базовый вариант структур...",""]];
